3013ff63086515c97944dd9187806e0edc1cac87
Nick <chaiwat@tencent.co.th>
2019-10-13 21:23:53 +0700

    [next-graph] refactor code

--- src/server.js
+++ src/server.js
@@ -1,24 +1,8 @@
 const next = require('next')
 const express = require('express')
-const { ApolloServer, gql } = require('apollo-server-express')
+const applyGraphQLmiddleware = require('./graph')
 
 const routes = require('./routes')
-
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
-};
-
-const graphServer = new ApolloServer({ typeDefs, resolvers });
 
 const port = 3000
 const app = next({dev: process.env.NODE_ENV !== 'production'})
@@ -26,7 +10,7 @@ const expressApp = express()
 const handler = routes.getRequestHandler(app)
 
 app.prepare().then(() => {
-  graphServer.applyMiddleware({ app: expressApp, path: '/api' });
+  applyGraphQLmiddleware({ app: expressApp })
 
   expressApp.use(handler).listen(port, function(err) {
     if (err) {
