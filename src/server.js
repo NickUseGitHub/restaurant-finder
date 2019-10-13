f34503f68005c4df2834406a14305558b56d9605
Nick <chaiwat@tencent.co.th>
2019-10-13 21:18:59 +0700

    [next-graph] apply apollo-server-express

--- src/server.js
+++ src/server.js
@@ -1,19 +1,32 @@
 const next = require('next')
 const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
+
 const routes = require('./routes')
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const graphServer = new ApolloServer({ typeDefs, resolvers });
 
 const port = 3000
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const expressApp = express()
 const handler = routes.getRequestHandler(app)
 
-// With express
 app.prepare().then(() => {
-  expressApp.use(function (req, res, next) {
-    console.log('yesss it middleware')
-
-    next()
-  })
+  graphServer.applyMiddleware({ app: expressApp, path: '/api' });
 
   expressApp.use(handler).listen(port, function(err) {
     if (err) {
