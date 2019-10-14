d3253e450cdd6c6e221c1a010c1a314d0f9bda40
Nick <chaiwat@tencent.co.th>
2019-10-14 20:45:17 +0700

    [app-layout] create menu & navbar

---
+++ src/components/layout/Menu.js
@@ -0,0 +1,9 @@
+import { Link } from '../../routes';
+
+export default function Menu(props) {
+  return (
+    <Link {...props}>
+      <a>{props.title}</a>
+    </Link>
+  );
+}
--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import Menu from './Menu';
 
 export default function NavBar() {
-  return <div>Header</div>;
+  return (
+    <nav className="navbar navbar-default">
+      <ul>
+        <Menu title="Home" route="home" />
+        <Menu title="Restaurants" route="restaurants" />
+      </ul>
+    </nav>
+  );
 }
