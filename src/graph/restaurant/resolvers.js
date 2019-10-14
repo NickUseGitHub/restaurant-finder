e9ae0436599d965dcd4876f15f4b99acfdf2d13a
Nick <chaiwat@tencent.co.th>
2019-10-14 21:38:17 +0700

    [restaurant] create more fields in type Restaurant

--- src/graph/restaurant/resolvers.js
+++ src/graph/restaurant/resolvers.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const get = require('lodash/get');
 
 module.exports = {
   Query: {
@@ -6,9 +7,20 @@ module.exports = {
       return axios({
         method: 'get',
         url: `${process.env.API_URL}/getRestaurants`,
-      }).then(response => {
-        return response.data;
-      });
+      })
+        .then(function(response) {
+          return response.data;
+        })
+        .then(function validateEachRestaurant(restaurants) {
+          return restaurants.map(function tranformRestaurant(restaurant) {
+            return {
+              name: get(restaurant, 'name'),
+              isOpenNow: get(restaurant, 'opening_hours.open_now') || false,
+              address: get(restaurant, 'vicinity'),
+              rating: get(restaurant, 'rating'),
+            };
+          });
+        });
     },
   },
 };
