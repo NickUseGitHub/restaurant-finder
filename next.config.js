8a356711211064573bf82e7999e43deb216375bd
Nick <chaiwat@tencent.co.th>
2019-10-14 21:48:56 +0700

    [restaurant] bind env on build time

--- next.config.js
+++ next.config.js
@@ -5,4 +5,10 @@ const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({
   dir: path.resolve(__dirname, './src'),
+  env: {
+    PORT: process.env.PORT,
+    API_URL: process.env.API_URL,
+    GRAPH_URL: process.env.GRAPH_URL,
+    GRAPH_PORT: process.env.GRAPH_PORT,
+  },
 });
