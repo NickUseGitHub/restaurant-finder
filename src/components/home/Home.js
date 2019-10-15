66d99671420e5d7d79f4f2e9b94e7fc27c22a6ca
Nick <chaiwat@tencent.co.th>
2019-10-15 23:53:56 +0700

    [cv] create RightPanel

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import LeftPanel from './LeftPanel';
+import RightPanel from './RightPanel';
 
 export default function Home() {
   return (
     <div className="home col-lg-12">
       <div className="row">
         <LeftPanel />
+        <RightPanel />
       </div>
     </div>
   );
---
+++ src/components/home/RightPanel.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function RightPanel() {
+  return <div className="righ-panel col-lg-8">Right Panel</div>;
+}
